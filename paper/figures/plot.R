33af76cc72e4cc48e6594e777d7b723abcb0eda9
Alan <aritter@aritter-UX31E.(none)>
2012-08-25 11:00:14 -0700

    some more writing...

--- paper/figures/plot.R
+++ paper/figures/plot.R
@@ -4,7 +4,7 @@ data = data[data$corpus == 1,]
 pdf('bleupinc1.pdf', width=6, height=6)
 
 plot(data$PINC, data$BLEU, pch=1:dim(data)[1], xlab='PINC', ylab='BLEU')
-title("Corpus 1")
+title("Sparknotes")
 legend("bottomleft", legend=data$system, pch=1:dim(data)[1])
 
 dev.off()
@@ -15,7 +15,7 @@ data = data[data$corpus == 2,]
 pdf('bleupinc2.pdf', width=6, height=6)
 
 plot(data$PINC, data$BLEU, pch=1:dim(data)[1], xlab='PINC', ylab='BLEU')
-title("Corpus 2")
+title("Enotes")
 legend("bottomleft", legend=data$system, pch=1:dim(data)[1])
 
 dev.off()
