1bc49b71943bb02f18a6775b56366d061bbe9e65
Alan <aritter@aritter-UX31E.(none)>
2012-08-31 23:21:38 -0700

    Added wei's new numbers

--- paper/figures/plot_style.R
+++ paper/figures/plot_style.R
@@ -11,15 +11,16 @@
 pdf('style_metrics.pdf', width=8, height=5)
 
 systems = c('16plays36LM', 'Dictionary', 'Video Baseline')
-metrics = c('Cosine Similarity', 'Logistic Regression')
+metrics = c('Cosine Similarity', 'Language Model', 'Logistic Regression')
 
 barplot(t(rbind(
 	 c(0.512979241631, 0.491599098577, 0.480059827357),
+	 c(0.50886, 0.30984, 0.21186),
 	 c(0.701862536235, 0.50375691204, 0.456308085791))),
 	names.arg=metrics, beside=TRUE)
 
 title("Average Style Metrics")
 
-legend("topright", legend=systems, fill=(gray.colors(n=3)))
+legend("topleft", legend=systems, fill=(gray.colors(n=3)))
 
 dev.off()
--- paper/figures/shakespere_to_modern.R
+++ paper/figures/shakespere_to_modern.R
@@ -1,14 +1,15 @@
 systems = c('16plays_16LM', 'Dictionary', 'Video Baseline')
-metrics = c('BLEU', 'PINC', 'Style-MaxEnt', 'Style-Cosine')
+metrics = c('BLEU', 'PINC', 'Style-MaxEnt', 'Style-LM', 'Style-Cosine')
 BLEU = c(27.79, 19.79, 24.23)
 PINC = c(48.70, 39.94, 4.09)
 sMaxEnt = c(71.99, 54.92, 44.02)
 sCosine = c(48.30, 46.11, 44.04)
+sLM = c(64.96, 33.59, 10.25)
 
 pdf('shakespeare_to_modern.pdf', width=8, height=5)
 
 barplot(
-	t(rbind(BLEU, PINC, sMaxEnt, sCosine)),
+	t(rbind(BLEU, PINC, sMaxEnt, sLM, sCosine)),
 	names.arg=metrics, beside=TRUE)
 title("Automatic Evaluation of Paraphrasing Shakespeare's plays to Modern English")
 
